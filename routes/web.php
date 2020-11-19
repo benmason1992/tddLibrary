c5157292f8e562d12c56f6edd61fec3c17db10c2
Ben <benmason1992@hotmail.co.uk>
2020-11-19 16:17:48 +0000

    Updated routes with Book destroy and Author store.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\AuthorsController;
 
 
 /*
@@ -21,4 +22,8 @@ Route::get('/', function () {
 
 Route::post('/books', [BooksController::class, 'store']);
 Route::patch('/books/{book}', [BooksController::class, 'update']);
+Route::delete('/books/{book}', [BooksController::class, 'destroy']);
+
+Route::post('/author', [AuthorsController::class, 'store']);
+
  
