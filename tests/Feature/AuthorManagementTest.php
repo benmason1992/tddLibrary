ebbb2d68584477653f4701680c1986f375a3f60d
Ben <benmason1992@hotmail.co.uk>
2020-11-19 16:17:21 +0000

    Started adding author functionality with the author store method.

---
+++ tests/Feature/AuthorManagementTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Author;
+use Carbon\Carbon;
+
+class AuthorManagementTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_an_author_can_be_created()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->post('/author', [
+            'name' => 'Author Name',
+            'dob' => '04/01/1992'
+        ]);
+
+        $author = Author::all();
+
+        $this->assertCount(1, $author);
+        $this->assertInstanceOf(Carbon::class, $author->first()->dob);
+        $this->assertEquals('1992/01/04', $author->first()->dob->format('Y/d/m'));
+    }
+}
